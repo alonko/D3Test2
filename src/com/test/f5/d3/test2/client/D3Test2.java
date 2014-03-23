592b9a97fe042249462139ce77ae5f91c9fc99e7
Alon Kodner <kodneralon@gmail.com>
2014-03-23 12:00:41 +0200

    Test 22

--- src/com/test/f5/d3/test2/client/D3Test2.java
+++ src/com/test/f5/d3/test2/client/D3Test2.java
@@ -42,7 +42,7 @@ public class D3Test2 implements EntryPoint {
 	public void onModuleLoad() {
 		final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
+		nameField.setText("User");
 		final Label errorLabel = new Label();
 
 		// We can add style names to widgets
